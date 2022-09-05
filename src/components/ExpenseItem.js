1a9d7a9b0009974936336f76b16aeabc9c1d7043
leyuri <leyuri97@gmail.com>
2022-09-05 13:00:40 +0900

    Fix : typo

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,18 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-    // const expenseDate = new Date(2022, 8, 19)
-    // const expenseTitle = 'Car Insurance';
-    // const expenseAmount = 294.67;
+  // const expenseDate = new Date(2022, 8, 19)
+  // const expenseTitle = 'Car Insurance';
+  // const expenseAmount = 294.67;
 
-    return (
-        <div className='expense-item'>
-            <div>{props.date}</div>
-            <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <div className='expense-item__price'>${props.amount}</div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 export default ExpenseItem;
